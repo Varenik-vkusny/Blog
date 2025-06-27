6f4eb3ca2b184d5485318ba80d883a908f41d94e
Varenik-vkusny <akybaevtimur7@gmail.com>
2025-06-27 18:03:58 +0500

    Add inits in blueprints and base skelet

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -26,6 +26,14 @@ def create_app():
     migrate.init_app(app, db)
     csrf.init_app(app)
 
+    from .main_bp import main_bp
+    from .register_bp import register_bp
+    from .auth_bp import auth_bp
+    from .add_bp import add_bp
 
+    app.register_blueprint(main_bp, url_prefix='/home')
+    app.register_blueprint(register_bp, url_prefix='/register')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(add_bp, url_prefix='/add')
 
     return app
