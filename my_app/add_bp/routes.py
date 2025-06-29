069623a4e5db35b54f7fbf0d0b8281eba0ba12a0
Varenik-vkusny <akybaevtimur7@gmail.com>
2025-06-29 14:04:24 +0500

    Add login_required to adding article

--- my_app/add_bp/routes.py
+++ my_app/add_bp/routes.py
@@ -3,9 +3,10 @@ from flask import render_template, redirect, flash, url_for
 from my_app import db
 from my_app.forms import PostForm
 from my_app.models import Post
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 @add_bp.route('/add', methods=['GET', 'POST'])
+@login_required
 def add():
     form = PostForm()
     if form.validate_on_submit():
